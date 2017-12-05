bec11739a0d7d0defae2a96abf038b306394c030
Michael Glass <me@mike.is>
2017-12-05 21:55:43 +0800

    part 2 but awfuls low

--- day05.hs
+++ day05.hs
@@ -12,8 +12,8 @@ data Maze = Maze {index        :: Int
 mazeInput :: Maze
 mazeInput= Maze 0 testInput
 
-nextStep :: Maze -> Maze
-nextStep (Maze index instructions)
+nextStep :: (Int -> Int) -> Maze -> Maze
+nextStep offset (Maze index instructions)
   | mazeFinished (Maze index instructions) = Maze index instructions -- do nothing
   | otherwise = Maze nextIndex nextInstructions
     where
@@ -21,24 +21,30 @@ nextStep (Maze index instructions)
       nextIndex = indexVal + index
       prefix = take index instructions
       suffix = drop (index + 1) instructions
-      nextInstructions = prefix ++ [indexVal + 1] ++ suffix
+      nextInstructions = prefix ++ [indexVal + (offset indexVal)] ++ suffix
 
 mazeFinished (Maze index instructions) = index < 0 || index >= length instructions
 
-numStepsTillCompletion maze
+numStepsTillCompletion offset maze
   | mazeFinished maze = 0
-  | otherwise = 1 + (numStepsTillCompletion . nextStep) maze
+  | otherwise = 1 + numStepsTillCompletion offset (nextStep offset maze)
 
-numToCompletion :: Integer
-numToCompletion = numStepsTillCompletion mazeInput
+step1Offset _ = 1
+
+step2Offset x
+  | x >= 3 = -1
+  | otherwise = 1
+
+numToCompletion1 = numStepsTillCompletion step1Offset
+numToCompletion2 = numStepsTillCompletion step2Offset
 
 parseToMaze :: String -> Maze
 parseToMaze str =
   Maze 0 (map read (lines str))
 
-
 main :: IO ()
 main =
   do
     testInput <- readFile "day05/input.txt"
-    print . numStepsTillCompletion . parseToMaze $ testInput
+    print . numToCompletion1 . parseToMaze $ testInput
+    print . numToCompletion2 . parseToMaze $ testInput
